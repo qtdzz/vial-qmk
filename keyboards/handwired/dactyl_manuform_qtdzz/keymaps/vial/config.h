ca8ceb1a7ac511f3431d4883878623e75ce956f1
Tien Nguyen <nqtien.dev@gmail.com>
2025-05-21 22:35:26 +0200

    Make qtdzz dactyl manuform 16 layers

--- keyboards/handwired/dactyl_manuform_qtdzz/keymaps/vial/config.h
+++ keyboards/handwired/dactyl_manuform_qtdzz/keymaps/vial/config.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#define DYNAMIC_KEYMAP_LAYER_COUNT 16
 #define VIAL_KEYBOARD_UID {0x2F, 0xA8, 0xA1, 0x3A, 0xBD, 0xD1, 0x66, 0xC9}
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
