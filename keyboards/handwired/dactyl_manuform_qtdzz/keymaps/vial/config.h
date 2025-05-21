3b664c816a03bcabfa095a5d8a1d19428c7dc412
Tien Nguyen <nqtien.dev@gmail.com>
2025-05-21 22:24:18 +0200

    Add qtdzz dactyl manuform

---
+++ keyboards/handwired/dactyl_manuform_qtdzz/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x2F, 0xA8, 0xA1, 0x3A, 0xBD, 0xD1, 0x66, 0xC9}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
